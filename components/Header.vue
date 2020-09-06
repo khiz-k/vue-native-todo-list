60e677ab63111ad9f6253616704d80a7e26dda33
khizar-k <khiz_k@outlook.com>
2020-09-06 15:47:30 -0700

    init

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="header">
+    <text class="title">{{ title }}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style>
+.header {
+  height: 60px;
+  background-color: #323232;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  color: #f3f3f3;
+  font-size: 28px;
+  font-weight: 900;
+  text-transform: uppercase;
+}
+</style>
+
+
